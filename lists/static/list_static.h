21bd4ae1e01349381568d6955ff031f2373167ad
GabrielDiaz-stack <francogabrielayaladiaz@gmail.com>
2026-03-27 15:27:15 -0300

    feat: add list_static.h struct and core operation

--- lists/static/list_static.h
+++ lists/static/list_static.h
@@ -1,6 +1,23 @@
 #ifndef LIST_STATIC_H
 #define LIST_STATIC_H
 
+//Array Structure
+typedef struct {
+    int* array;
+    int capacity;
+    int size;
+}List_static;
 
+//Funtion prototype
+List_static* create(int capacity);  //Initialize empty list
+List_static* insert(List_static* list, int position, int element);  //Insert element at position
+List_static* insertEnd(List_static* list, int element); //Insert element at the end
+List_static* delete(List_static* list, int element);    //Delete element by position
+int search(List_static* list, int value);   //Search element by value
+void display(List_static* list);    //Display all elements
+int isEmpty(List_static* list);     //Check if list is empty
+int isFull(List_static* list);      //Check if list is full
+int length(List_static* list);      //Get current numbers of elements
+int getElement(List_static* list, int position);    //Get element by position
 
 #endif
